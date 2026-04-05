dfbff0653392587bbc989f302eb55522d100dc18
Kishore Kumar v <kishore.kumarkv93@gmail.com>
2026-04-05 08:42:31 +0530

    Packages installed and setup file updated

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(name='Agentic-Trading-System',
+      author="Kishore",
+      author_email="kishore.kumarkv93@gmail.com",
+      packages=find_packages(),
+      install_requires=['langchain','lancedb','langgraph'])
